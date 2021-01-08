01b6653b18c07c781fa8c2f027d1080f593fb02b
Philipp Tobias Christian Kant <ptckant@gmail.com>
2021-01-08 19:04:25 +0100

    ex 3.3. (phonebook backend step 3) done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+//app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -24,14 +26,23 @@ let persons = [
   },
 ]
 
-app.use(express.json())
-
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
 app.get('/info', (request, response) => {
   response.send(`Phonebook has info for ${persons.length} people<p>${new Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
