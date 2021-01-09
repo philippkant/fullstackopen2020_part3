18ff2262567cd8542e65def72c6c5ce909c0046a
Philipp Tobias Christian Kant <ptckant@gmail.com>
2021-01-09 12:30:36 +0100

    ex 3.7. (phonebook backend step 7) done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
   {
@@ -77,7 +80,6 @@ app.post('/api/persons', (request, response) => {
 
   response.json(new_person)
 })
-
 
 const PORT = 3001
 app.listen(PORT, () => {
