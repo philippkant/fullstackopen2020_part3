9a784d8ff105f385895ba885bb158f61ffbd8875
Philipp Tobias Christian Kant <ptckant@gmail.com>
2021-01-10 12:10:23 +0100

    ex 3.9. step 9 (phonebook backend) done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data' ))
@@ -82,7 +83,7 @@ app.post('/api/persons', (request, response) => {
   response.json(new_person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
